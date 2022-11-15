2c4b5a5c05ee5dedf359b175befbb075626d10aa
fredaadifardana <fredadifar@gmail.com>
2022-11-15 21:12:34 +0700

    update

--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' show Client, Response;
 import 'package:minggu_10_moviedb/model/popular_movie.dart';
 
 class ApiProvider {
-  String apiKey = '0fc5740199faa752da813c8c97f659e8';
+  String apiKey = 'b2c8fc324d4e7877a1d74a47576394d5';
   String baseUrl = 'https://api.themoviedb.org/3';
 
   Client client = Client();
@@ -12,7 +12,7 @@ class ApiProvider {
     String url = '$baseUrl/movie/popular?api_key=$apiKey';
     // print(url);
 
-    Response response = await client.get();
+    Response response = await client.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       return PopularMovies.fromJson(jsonDecode(response.body));
